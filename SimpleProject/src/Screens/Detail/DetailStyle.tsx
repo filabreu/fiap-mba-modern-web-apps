cc7ceadb157e646765c4f4cf2a077e7eb379d83b
brunoxti <bruno.xti@outlook.com>
2022-07-09 15:20:04 -0300

    update Details

--- SimpleProject/src/Screens/Detail/DetailStyle.tsx
+++ SimpleProject/src/Screens/Detail/DetailStyle.tsx
@@ -8,14 +8,15 @@ export const MainGrid = styled(Grid)`
     text-align: center;
   }
 `;
-export const TitlePage = styled(Typography)`
-  && {
-    font-size: 28px;
-  }
-`;
 
 export const ButtonMaxSize = styled(Button)`
   && {
     max-width: 200px;
   }
 `;
+
+export const MapStyle = {
+  position: "relative",
+  width: "48%",
+  height: "100%",
+};
