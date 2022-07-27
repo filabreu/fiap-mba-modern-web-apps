39d929edbe2853fae8a423556c3a974671781284
Filipe Abreu <filabreu@gmail.com>
2022-07-27 09:51:52 -0300

    Filter out empty fields from data passed to HTTP client

--- services/http.ts
+++ services/http.ts
@@ -1,3 +1,9 @@
+const filterDataFields = (data: object) => (
+  Object.keys(data)
+    .filter((k): boolean => !!data[k as keyof object])
+    .reduce((a, k) => ({ ...a, [k]: data[k as keyof object] }), {})
+)
+
 const request = async (method: string, url: string, data?: URLSearchParams | null, headers?: HeadersInit): Promise<object> => {
   let authHeaders = {}
 
@@ -34,13 +40,36 @@ const request = async (method: string, url: string, data?: URLSearchParams | nul
 }
 
 const get = async (url: string, data?: object, headers?: HeadersInit): Promise<object> => (
-  request('GET', url + (data ? `/?${new URLSearchParams(Object.entries(data)).toString()}` : null), null, headers)
+  request(
+    'GET',
+    url + (
+      data
+        ? `/?${new URLSearchParams(Object.entries(filterDataFields(data))).toString()}`
+        : null
+    ),
+    null,
+    headers
+  )
 )
 const post = async (url: string, data?: object, headers?: HeadersInit): Promise<object> => (
-  request('POST', url, data ? new URLSearchParams(Object.entries(data)) : null, headers)
+  request(
+    'POST',
+    url,
+    data
+      ? new URLSearchParams(Object.entries(filterDataFields(data)))
+      : null,
+    headers
+  )
 )
 const put = async (url: string, data?: object, headers?: HeadersInit): Promise<object> => (
-  request('PUT', url, data ? new URLSearchParams(Object.entries(data)) : null, headers)
+  request(
+    'PUT',
+    url,
+    data
+      ? new URLSearchParams(Object.entries(filterDataFields(data)))
+      : null,
+    headers
+  )
 )
 const destroy = async (url: string, data?: object, headers?: HeadersInit): Promise<object> => (
   request('DELETE', url, null, headers)
